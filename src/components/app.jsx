e7add414e5dd1b1e9929e6fe6c868f43faa05be1
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-23 22:17:24 +0000

    trying to investigate, stuck with forecast-summary

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import LocationDetails from './location-details'
 import PropTypes from 'prop-types';
-
+import LocationDetails from './location-details';
+import ForecastSummaries from './forecast-summaries';
 
 const App = props => (
-    <LocationDetails
-        city={props.location.city}
-        country={props.location.country}
-    />
+    <div className="forecast">
+        <LocationDetails
+            city={props.location.city}
+            country={props.location.country}
+        />
+
+        <ForecastSummaries forecasts={props.forecasts} />
+    </div>
 );
 
 App.propTypes = {
     location: PropTypes.shape({
         city: PropTypes.string,
-        country: PropTypes.string
+        country: PropTypes.string,
     }).isRequired,
+    forecasts: PropTypes.array.isRequired,
 };
-
 
 export default App;
