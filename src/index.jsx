92df078700277936010afbfee91e051a00510bcc
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-23 13:02:11 +0000

    imported weather data, creating location details

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
+import { location } from './data/forecast'
 
-render(<App />, global.document.getElementById('root'));
+render(<App location={location} />, global.document.getElementById('root'));
