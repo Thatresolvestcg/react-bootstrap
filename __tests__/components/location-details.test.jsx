5ac2b6a3a8985abd55240c93aa93d55588d11699
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-23 17:06:00 +0000

    experimenting with tests

--- __tests__/components/location-details.test.jsx
+++ __tests__/components/location-details.test.jsx
@@ -3,10 +3,11 @@ import Enyzyme from 'enzyme';
 import LocationDetails from '../../src/components/location-details';
 
 describe('Component', () => {
-    test('renders the passed city and country in a H1 tag', () => {
-        const wrapper = Enyzyme.shallow((
-            <LocationDetails city='foo' country='bar' />
+    it('renders the passed city and country in a h1 tag', () => {
+        const wrapper = Enzyme.shallow((
+            <LocationDetails city="foo" country="bar" />
         ));
+
         const text = wrapper.find('h1').text();
 
         expect(text).toEqual('foo, bar');
