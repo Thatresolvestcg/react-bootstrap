1803827401abd4e6f1f5f0d0c76c0f40ab1970b7
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-23 13:34:09 +0000

    location details now completed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import LocationDetails from './location-details'
+import PropTypes from 'prop-types';
 
-const App = () => <h1>Hello World</h1>;
+
+const App = props => (
+    <LocationDetails
+        city={props.location.city}
+        country={props.location.country}
+    />
+);
+
+App.propTypes = {
+    location: PropTypes.shape({
+        city: PropTypes.string,
+        country: PropTypes.string
+    }).isRequired,
+};
 
 
 export default App;
