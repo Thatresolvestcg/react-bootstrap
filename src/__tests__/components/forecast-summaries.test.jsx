3daf4a8939efaa2769fb4fbd6c121521d757bc93
thatresolvestcg <thatresolvestcg@gmail.com>
2019-11-25 22:10:54 +0000

    tests now run but having pathing issues for forecast-summary

--- src/__tests__/components/forecast-summaries.test.jsx
+++ src/__tests__/components/forecast-summaries.test.jsx
@@ -1,55 +1,57 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ForecastSummary from '../components/forecast-summaries.jsx';
+import ForecastSummary from '../src/components/forecast-summary.jsx';
 
-it('renders the date', () => {
-    const wrapper = shallow((
-        <ForecastSummary
-            date="mockDate"
-            temperature="mockTemperature"
-            description="mockDescription"
-            icon="mockIcon"
-        />
-    ));
+describe('Component', () => {
+    it('renders the date', () => {
+        const wrapper = shallow((
+            <ForecastSummary
+                date="mockDate"
+                temperature="mockTemperature"
+                description="mockDescription"
+                icon="mockIcon"
+            />
+        ));
 
-    expect(wrapper.find('.forecast-summary__date').text()).toEqual('mockDate');
-});
+        expect(wrapper.find('.forecast-summary__date').text()).toEqual('mockDate');
+    });
 
-it('renders the temperature', () => {
-    const wrapper = shallow((
-        <ForecastSummary
-            date="mockDate"
-            temperature="mockTemperature"
-            description="mockDescription"
-            icon="mockIcon"
-        />
-    ));
+    it('renders the temperature', () => {
+        const wrapper = shallow((
+            <ForecastSummary
+                date="mockDate"
+                temperature="mockTemperature"
+                description="mockDescription"
+                icon="mockIcon"
+            />
+        ));
 
-    expect(wrapper.find('.forecast-summary__temperature').text()).toEqual('mockTemperature');
-});
+        expect(wrapper.find('.forecast-summary__temperature').text()).toEqual('mockTemperature');
+    });
 
-it('renders the description', () => {
-    const wrapper = shallow((
-        <ForecastSummary
-            date="mockDate"
-            temperature="mockTemperature"
-            description="mockDescription"
-            icon="mockIcon"
-        />
-    ));
+    it('renders the description', () => {
+        const wrapper = shallow((
+            <ForecastSummary
+                date="mockDate"
+                temperature="mockTemperature"
+                description="mockDescription"
+                icon="mockIcon"
+            />
+        ));
 
-    expect(wrapper.find('.forecast-summary__description').text()).toEqual('mockDescription');
-});
+        expect(wrapper.find('.forecast-summary__description').text()).toEqual('mockDescription');
+    });
 
-it('renders the icon', () => {
-    const wrapper = shallow((
-        <ForecastSummary
-            date="mockDate"
-            temperature="mockTemperature"
-            description="mockDescription"
-            icon="mockIcon"
-        />
-    ));
+    it('renders the icon', () => {
+        const wrapper = shallow((
+            <ForecastSummary
+                date="mockDate"
+                temperature="mockTemperature"
+                description="mockDescription"
+                icon="mockIcon"
+            />
+        ));
 
-    expect(wrapper.find('.forecast-summary__icon').text()).toEqual('mockIcon');
+        expect(wrapper.find('.forecast-summary__icon').text()).toEqual('mockIcon');
+    });
 });
